7539345b975a3a13dc1d71ec590b7e9b8f5d2e4e
KingKush99 <92454981+KingKush99@users.noreply.github.com>
2025-09-01 08:43:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Add this line
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
-  base: '/team-sports-shell/' // Add this line
+  base: '/team-sports-shell/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Add this block
+    },
+  },
 })
