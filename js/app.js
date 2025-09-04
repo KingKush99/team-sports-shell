4b5d7311fc66dc87c9efb460caf0e2043eddff14
KingKush99 <92454981+KingKush99@users.noreply.github.com>
2025-09-04 02:21:48 -0400

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-  document.querySelectorAll('.panel').forEach(panel=>{
-    document.addEventListener('click', e=>{
-      if(!panel.contains(e.target)) panel.style.display='none';
-    });
-  });
-});
